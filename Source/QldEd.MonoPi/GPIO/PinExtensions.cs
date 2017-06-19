a6b2c0685cf2b04eb5a9537688b883b6162eeea4
qlded <qlded@users.noreply.github.com>
2017-06-19 20:47:12 +1000

    Added comments to better explain PinExtensions.

--- Source/QldEd.MonoPi/GPIO/PinExtensions.cs
+++ Source/QldEd.MonoPi/GPIO/PinExtensions.cs
@@ -2,6 +2,11 @@
 
 namespace QldEd.MonoPi.GPIO
 {
+    /// <summary>
+    /// Extension methods to allow you to initialise a <see cref="PreparedPin"/> from an enum value. 
+    /// This allows the pinouts to be simple enums but without the need for the consumer to cast to an int. 
+    /// In theory you could make the PreparedPin constructor internal to restrict consumers to known pin values.
+    /// </summary>
     public static class PinExtensions
     {
         public static PreparedPin Prepare(this Pi1Pins pin)
